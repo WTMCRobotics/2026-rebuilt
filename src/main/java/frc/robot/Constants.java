e345a2d1cb7aeba2e3cdc61f777ad10f303ac3b4
team6101 <wtmcrobotics@wccnet.edu>
2026-01-27 16:20:46 -0500

    Rename constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,6 +2,10 @@ package frc.robot;
 
 public class Constants {
     // Shooter
-    public static final int ShooterMotorID = 0;
-    public static final double ShooterSpeed = 1.0;
+    public static final int SHOOTER_MOTOR_ID = 0;
+    public static final double SHOOTER_SPEED = 1.0;
+
+    public static final double SHOOTER_P = 0.0;
+    public static final double SHOOTER_I = 0.0;
+    public static final double SHOOTER_D = 0.0;
 }
