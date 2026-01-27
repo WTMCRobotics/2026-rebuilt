6c139677eb48bbb5ef34542338f73c06fa39eafb
team6101 <wtmcrobotics@wccnet.edu>
2026-01-27 16:53:33 -0500

    Add Intake.java Command

---
+++ src/main/java/frc/robot/commands/Intake.java
@@ -0,0 +1,35 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IntakeSubsystem;
+
+import frc.robot.Constants;
+
+public class Intake extends Command {
+    IntakeSubsystem intakeSubsystem;
+    double speed;
+
+    public Intake(IntakeSubsystem intakeSubsystem, double speed) {
+        this.intakeSubsystem = intakeSubsystem;
+        this.speed = speed;
+    }
+
+    public void initialize() {
+        intakeSubsystem.setIntake(0);
+    }
+
+    public void execute() {
+        
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        intakeSubsystem.setIntake(0);
+    }
+}
