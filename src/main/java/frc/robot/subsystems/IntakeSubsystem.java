a11a156e7f5721190f8cab65e489bf35f47caf97
team6101 <wtmcrobotics@wccnet.edu>
2026-01-27 16:53:12 -0500

    Add IntakeSubsystem

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.Constants;
+
+public class IntakeSubsystem implements Subsystem {
+    SparkMax intake = new SparkMax(Constants.INTAKE_MOTOR_ID, MotorType.kBrushless);
+    SparkMax intakeExtender = new SparkMax(Constants.INTAKE_EXTENDER_ID, MotorType.kBrushless);
+
+    public void setExtender(double speed) {
+        intakeExtender.set(speed);
+    }
+
+    public void setIntake(double speed) {
+        intake.set(speed);
+    }
+
+    public double getEncoderPosition() {
+        return intakeExtender.getEncoder().getPosition();
+    }
+
+
+    public void resetEncoder() {
+        intakeExtender.getEncoder().setPosition(0);
+    }
+}
