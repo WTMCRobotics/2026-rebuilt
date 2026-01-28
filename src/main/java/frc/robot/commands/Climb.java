7c76009b4247c522833c4b5167bfd4fcc223aca9
team6101 <wtmcrobotics@wccnet.edu>
2026-01-28 18:46:42 -0500

    Add Climb Command

--- src/main/java/frc/robot/commands/Climb.java
+++ src/main/java/frc/robot/commands/Climb.java
@@ -2,34 +2,78 @@ package frc.robot.commands;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ClimbSubsystem;
 
 import frc.robot.Constants;
 
 public class Climb extends Command {
-    IntakeSubsystem intakeSubsystem;
-    double speed;
+    ClimbSubsystem climbSubsystem;
+    boolean up;
+    private final PIDController controllerLeft;
+    private final PIDController controllerRight;
 
-    public Climb(IntakeSubsystem intakeSubsystem, double speed) {
-        this.intakeSubsystem = intakeSubsystem;
-        this.speed = speed;
+
+    public Climb(ClimbSubsystem climbSubsystem, boolean up) {
+        this.climbSubsystem = climbSubsystem;
+        this.up = up;
+        
+        if(up) {
+            controllerLeft = new PIDController(
+            Constants.CLIMB_ARM_UP_P,
+            Constants.CLIMB_ARM_UP_I,
+            Constants.CLIMB_ARM_UP_D
+                );
+
+            controllerRight = new PIDController(
+            Constants.CLIMB_ARM_UP_P,
+            Constants.CLIMB_ARM_UP_I,
+            Constants.CLIMB_ARM_UP_D
+                );
+        } else {
+            controllerLeft = new PIDController(
+            Constants.CLIMB_ARM_DOWN_P,
+            Constants.CLIMB_ARM_DOWN_I,
+            Constants.CLIMB_ARM_DOWN_D
+                );
+
+            controllerRight = new PIDController(
+            Constants.CLIMB_ARM_DOWN_P,
+            Constants.CLIMB_ARM_DOWN_I,
+            Constants.CLIMB_ARM_DOWN_D
+                );
+        }
+
+
     }
+    
 
     public void initialize() {
-        intakeSubsystem.setIntake(0);
+        controllerLeft.setTolerance(Constants.CLIMB_ARM_TOLERANCE);
+        controllerRight.setTolerance(Constants.CLIMB_ARM_TOLERANCE);
+        if (up) {
+            controllerLeft.setSetpoint(Constants.CLIMB_ARM_TARGET);
+            controllerRight.setSetpoint(Constants.CLIMB_ARM_TARGET);
+        } else {
+            controllerLeft.setSetpoint(Constants.CLIMB_ARM_ZERO);
+            controllerRight.setSetpoint(Constants.CLIMB_ARM_ZERO); 
+        }
+
     }
 
     public void execute() {
-        
+        climbSubsystem.setClimbLeft(controllerLeft.calculate(climbSubsystem.getClimbLeftPosition()));
+        climbSubsystem.setClimbRight(controllerRight.calculate(climbSubsystem.getClimbRightPosition()));
+
     }
 
     @Override
     public boolean isFinished() {
-        return false;
+        return controllerLeft.atSetpoint() && controllerRight.atSetpoint();
     }
 
     @Override
     public void end(boolean interrupted) {
-        intakeSubsystem.setIntake(0);
+        climbSubsystem.setClimbLeft(0);
+        climbSubsystem.setClimbRight(0);
     }
 }
